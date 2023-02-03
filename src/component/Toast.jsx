7a732191fd0e1483e3da0a95892ebfcceec59110
Divish28 <divish.karthik@aspiresys.com>
2023-02-03 17:58:07 +0530

    Creating Seperate toast Component

--- src/component/Toast.jsx
+++ src/component/Toast.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 import {toast,ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 
+const showToastMessage = () => {
+    toast.success('Shortlisted !', {
+        position: toast.POSITION.TOP_CENTER
+    });
+};
+
+const showToastErrorMessage = () => {
+  toast.error(`Login to view details`, {
+      position: toast.POSITION.TOP_CENTER
+  });
+};
+
+const showToastShortlistMessage = () => {
+  toast.error('Login to Shortlist the House', {
+      position: toast.POSITION.TOP_CENTER
+  });
+};
 const Toast = () => {
 
 }
